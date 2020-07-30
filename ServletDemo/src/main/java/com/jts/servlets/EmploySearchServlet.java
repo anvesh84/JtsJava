0c70179266b35c465ac280f2b9dcc5272f640a12
Prasanna <prasanna@hexaware.com>
2020-07-30 09:12:49 +0530

    servlets

---
+++ ServletDemo/src/main/java/com/jts/servlets/EmploySearchServlet.java
@@ -0,0 +1,61 @@
+package com.jts.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EmploySearchServlet
+ */
+public class EmploySearchServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EmploySearchServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int empno = Integer.parseInt(request.getParameter("empno"));
+		Connection con = ConnectionHelper.getConnection();
+		PrintWriter out = response.getWriter();
+		String cmd = "SELECT * FROM EMPLOY WHERE Empno=?";
+		try {
+			PreparedStatement pst = con.prepareStatement(cmd);
+			pst.setInt(1, empno);
+			ResultSet rs = pst.executeQuery();
+			if (rs.next()) {
+				out.println("Employ Name  " +rs.getString("name") + "<br/>");
+				out.println("Employ Department  " +rs.getString("dept") + "<br/>");
+				out.println("Employ Designation  " +rs.getString("desig") + "<br/>");
+				out.println("Salary  " +rs.getInt("basic") + "<br/>");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
